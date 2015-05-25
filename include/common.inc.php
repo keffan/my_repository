6592fb216942a99b2fad173f2653934011a8be90
Keffan <keffan@gmail.com>
2015-05-25 22:11:07 +0300

    Изменил загрузку файлов на сервер

--- include/common.inc.php
+++ include/common.inc.php
@@ -4,3 +4,7 @@
     require_once(SMARTY_DIR . 'Smarty.class.php');
     require_once('/include/request.inc.php');
     require_once('/include/survey.inc.php');
+    require_once('include/upload.inc.php');
+    require_once('/include/database.inc.php');
+    require_once('/include/dvd.inc.php');
+    dbConnect();
--- include/upload.inc.php
+++ include/upload.inc.php
@@ -1,23 +1,18 @@
 <?php
-    function upload_file($files)
+    function GetFName($files)
     {
-        $uploaddir = '../uploads/';
+        $uploaddir = 'uploads/';
         $savedfile = $uploaddir . $files['filename']['name'];
         $file = $files['filename']['tmp_name'];
+        return (array($file, $savedfile));
+    }
+
+    function UploadFile($files)
+    {
         if ($files['filename']['error'] > 0)
         {
-            $messages = array
-            (
-                1 => 'Размер файла превышает допустимое значение UPLOAD_MAX_FILE_SIZE',
-                2 => 'Размер файла превышает допустимое значение MAX_FILE_SIZE',
-                3 => 'Не удалось загрузить часть файла',
-                4 => 'Файл не был загружен',
-                6 => 'Отсутствует временная папка.',
-                7 => 'Не удалось записать файл на диск.',
-                8 => 'PHP-расширение остановило загрузку файла.'
-            );
             return $messages[$files['filename']['error']];
         }    
-        return (@move_uploaded_file($file, $savedfile)) ? "Файл успешно загружен на сервер" 
-                                                       : "Не удалось загрузить файл";
+        $fName = GetFName($files);
+        return (@move_uploaded_file($fName[0], $fName[1])) ? 0 : 9;
     }
